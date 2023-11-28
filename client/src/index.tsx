fbbaf2ba3dff420aaf24100cf2956d35682952f5
daichangxin <daichangxin@gmail.com>
2023-11-28 09:13:59 +0800

    feat: adjust from eds-ui

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,6 @@ import { createRoot } from 'react-dom/client';
 import { Index } from './modules/index';
 
 import './assets/css/tailwind.css';
-import '@eds-open/eds-ui/dist/tailwind.css';
 import '@eds-open/eds-ui/dist/styles.css';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);

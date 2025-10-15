232c9d7354d8f599024ba8c81571556e7652c5fc
daichangxin <daichangxin@gmail.com>
2025-10-15 09:37:07 +0800

    feat: add error boundary

--- client/src/pages/routes/index.tsx
+++ client/src/pages/routes/index.tsx
@@ -3,27 +3,30 @@ import type { FC } from 'react';
 import { Suspense } from 'react';
 import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
 
+import { AppErrorBoundary } from '@/components/AppErrorBoundary';
 import { AppLayout } from '@/components/AppLayout';
 
 import { Main } from '../home';
 
 export const AppRoutes: FC = () => {
     return (
-        <BrowserRouter>
-            <Toaster />
-            <Routes>
-                <Route
-                    path="/"
-                    element={(
-                        <Suspense fallback={<div />}>
-                            <AppLayout><Outlet /></AppLayout>
-                        </Suspense>
-                    )}
-                >
-                    <Route index element={<Main />} />
-                    <Route path="*" element={<div>Page Not Found</div>} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
+        <AppErrorBoundary>
+            <BrowserRouter>
+                <Toaster />
+                <Routes>
+                    <Route
+                        path="/"
+                        element={(
+                            <Suspense fallback={<div />}>
+                                <AppLayout><Outlet /></AppLayout>
+                            </Suspense>
+                        )}
+                    >
+                        <Route index element={<Main />} />
+                        <Route path="*" element={<div>Page Not Found</div>} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </AppErrorBoundary>
     );
 };
